0bc9b96725963addced8959b6aebd503b0d76660
hk12r <2024.himani.ruplani@ves.ac.in>
2026-09-03 12:01:01 +0530

    Wrap application with ReadoraProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { ReadoraProvider } from "./context/ReadoraContext";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/readora">
-      <App />
+      <ReadoraProvider>
+        <App />
+      </ReadoraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
